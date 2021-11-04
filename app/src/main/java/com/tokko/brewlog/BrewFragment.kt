d9368d298aaaf8fc46184a8be788bfdd1cc0f9c7
tokko <andreg@kth.se>
2021-11-04 16:03:14 +0100

    composified the detail pane

--- app/src/main/java/com/tokko/brewlog/BrewFragment.kt
+++ app/src/main/java/com/tokko/brewlog/BrewFragment.kt
@@ -1,128 +1,142 @@
 package com.tokko.brewlog
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.tokko.brewlog.databinding.BrewFragmentBinding
-import com.tokko.brewlog.databinding.DryHoppedItemBinding
-import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.GroupieViewHolder
-import com.xwray.groupie.viewbinding.BindableItem
-import org.joda.time.DateTime
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+import org.kodein.di.generic.instance
 import java.text.SimpleDateFormat
 import java.util.*
 
-class BrewFragment(private val fireStoreRepository: IFirestoreRepository) : Fragment() {
-    lateinit var brew: Brew
-    private val adapter = GroupAdapter<GroupieViewHolder>()
-    private var _binding: BrewFragmentBinding? = null
-    private val binding get() = _binding!!
+class BrewFragment : Fragment(), KodeinAware {
+    override val kodein by kodein()
+    private val fireStoreRepository: IFirestoreRepository by instance()
     private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("sv-SE"))
+    private val brewState = mutableStateOf(Brew())
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = BrewFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp)
+                ) {
+                    Text(
+                        text = brewState.value.name,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "Brew date: ",
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(text = dateFormat.format(brewState.value.brewDate))
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Divider()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "Fermentation end date: ",
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(text = dateFormat.format(brewState.value.fermentationTime))
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Divider()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Dry hops:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    LazyColumn {
+                        items(brewState.value.dryhops) { dryHop ->
+                            DryHop(dryHop)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Divider()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(text = "Bottled", modifier = Modifier.weight(1f))
+                        Checkbox(
+                            checked = brewState.value.isBottled,
+                            onCheckedChange = {
+                                brewState.value.isBottled = !brewState.value.isBottled
+                            })
+                    }
+                    brewState.value.bottledDate?.let {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text(
+                                text = "Bottled: ",
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Text(text = dateFormat.format(it))
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "Drinkable: ",
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(text = dateFormat.format(brewState.value.drinkable))
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun DryHop(dryHop: DryHopping) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(text = dateFormat.format(dryHop.date), modifier = Modifier.weight(1f))
+            Text(text = "Hop: ${dryHop.type}", modifier = Modifier.weight(1f))
+            Text(text = "Amount: ${dryHop.amount}g", modifier = Modifier.weight(1f))
+            Checkbox(
+                checked = dryHop.checked,
+                onCheckedChange = { dryHop.checked = !dryHop.checked })
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val brewId = (savedInstanceState ?: arguments)?.getString("brewId") ?: ""
         fireStoreRepository.getBrew(brewId) {
-            brew = it
-            initViews()
-        }
-    }
-
-    private fun initViews() {
-        binding.brewName.text = brew.name
-        binding.brewDate.text = dateFormat.format(Date(brew.brewDate))
-        binding.fermentationEndDate.text = dateFormat.format(Date(brew.fermentationTime))
-        binding.dryHopRecycler.adapter = adapter
-        binding.dryHopRecycler.layoutManager = LinearLayoutManager(activity)
-        adapter.addAll(brew.dryhops.map {
-            DryHoppedItem(it) {
-                fireStoreRepository.addBrew(brew)
-            }
-        })
-        adapter.notifyItemRangeChanged(0, adapter.groupCount)
-        binding.bottledCheckbox.isChecked = brew.isBottled
-        binding.bottledCheckbox.isEnabled = !brew.isBottled
-        binding.bottledCheckbox.setOnCheckedChangeListener { _, isChecked ->
-            brew.isBottled = isChecked
-            brew.bottledDate = DateTime.now().withTimeAtStartOfDay().millis
-            fireStoreRepository.addBrew(brew)
-            updateDrinkableDate()
-            binding.bottledCheckbox.isEnabled = !isChecked
-            brew.drinkable = DateTime.now().plusDays(14).millis
-            fireStoreRepository.addAlarm(
-                Alarm(
-                    brew.id,
-                    DateTime.now().plusDays(14).millis,
-                    "Drinkable brew!",
-                    "${brew.name} is now drinkable!"
-                ).also { brew.drinkableAlarmId = it.id })
-            fireStoreRepository.addBrew(brew)
-        }
-        binding.dryHopLabel.visibility = if (brew.dryhops.isNullOrEmpty()) GONE else VISIBLE
-        updateDrinkableDate()
-    }
-
-    fun updateDrinkableDate() {
-        if (brew.isBottled) {
-            binding.drinkableLabel.visibility = VISIBLE
-            binding.drinkableDate.visibility = VISIBLE
-            binding.bottledDateLabel.visibility = VISIBLE
-            binding.bottledDate.visibility = VISIBLE
-            binding.bottledDate.text = dateFormat.format(DateTime(brew.bottledDate).millis)
-            binding.drinkableDate.text =
-                dateFormat.format(DateTime(brew.bottledDate).plusDays(14).millis)
-        } else {
-            binding.drinkableLabel.visibility = GONE
-            binding.drinkableDate.visibility = GONE
-            binding.bottledDateLabel.visibility = GONE
-            binding.bottledDate.visibility = GONE
+            brewState.value = it
         }
     }
 
     companion object {
-        fun newInstance(id: String, firestoreRepository: IFirestoreRepository) =
-            BrewFragment(firestoreRepository).apply {
+        fun newInstance(id: String) =
+            BrewFragment().apply {
                 arguments = Bundle().apply { putString("brewId", id) }
             }
     }
-}
-
-class DryHoppedItem(val dryHopping: DryHopping, val saveCallback: () -> Unit) :
-    BindableItem<DryHoppedItemBinding>() {
-    val datePattern = SimpleDateFormat("yyyy-MM-dd", Locale("sv-SE"))
-    override fun initializeViewBinding(view: View): DryHoppedItemBinding {
-        return DryHoppedItemBinding.bind(view)
-    }
-
-    @SuppressLint("SetTextI18n")
-    override fun bind(viewBinding: DryHoppedItemBinding, position: Int) {
-        viewBinding.dryhopAmount.text = "Amount: ${dryHopping.amount}g"
-        viewBinding.dryHopDate.text = datePattern.format(Date(dryHopping.date))
-        viewBinding.dryHopType.text = "Hop: ${dryHopping.type}"
-        viewBinding.isDryhoppedCheckBox.isChecked = dryHopping.checked
-        viewBinding.isDryhoppedCheckBox.isEnabled = !dryHopping.checked
-
-        viewBinding.isDryhoppedCheckBox.setOnCheckedChangeListener { _, isChecked ->
-            dryHopping.checked = isChecked
-            saveCallback()
-        }
-    }
-
-    override fun getLayout() = R.layout.dry_hopped_item
-
 }
--- app/src/main/java/com/tokko/brewlog/MainActivity.kt
+++ app/src/main/java/com/tokko/brewlog/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
-import org.kodein.di.generic.instance
 
 class MainActivity : AppCompatActivity(), KodeinAware {
     override val kodein by kodein(this)
-    private val fireStoreRepository: IFirestoreRepository by instance()
     var brewList = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +34,7 @@ class MainActivity : AppCompatActivity(), KodeinAware {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         brewList = false
         supportFragmentManager.beginTransaction()
-            .replace(android.R.id.content, BrewFragment.newInstance(id, fireStoreRepository))
+            .replace(android.R.id.content, BrewFragment.newInstance(id))
             .commit()
     }
 
